1428b83799c2f00f9cabc08615209be4fa01b30c
bbbrandy <76755832+bbbrandy@users.noreply.github.com>
2020-12-29 23:07:09 +0000

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,6 @@
+module.exports = async (client) => {
+  console.log(`[API] Logged in as ${client.user.username}`);
+  await client.user.setActivity("-h help", {
+    type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+  });
+};
